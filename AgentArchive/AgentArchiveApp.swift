64ea11362dad1974266b2199126e3cacdd7b63df
SaarthakTuli <saarthaktuli@gmail.com>
2023-07-17 12:08:43 +0530

    Initial Commit

---
+++ AgentArchive/AgentArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentArchiveApp.swift
+//  AgentArchive
+//
+//  Created by Saarthak Tuli on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AgentArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
